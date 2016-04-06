92e519a0b9de4d76b263b070fe4dc4455f90c10f
Niels Snakenborg <nc.snakenborg@student.avans.nl>
2016-04-06 21:13:03 +0200

    update

--- public/javascripts/race.js
+++ public/javascripts/race.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         populateCafeTable();
     }
 
-    SOCKET = io.connect("http://localhost");
+    SOCKET = io.connect("https://kroegentochtncsnaken.herokuapp.com");
     SOCKET.on('RaceUpdate', function (data) {
         RACEID = data.id;
         populateCafeTable();
